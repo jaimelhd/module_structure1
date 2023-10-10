404260973fdd037118a7f95376e9a6477bffbe56
jaimelhd <jaimelhd9@gmail.com>
2023-10-10 10:07:50 +0200

    updatee

--- module_structure1/__init__.py
+++ module_structure1/__init__.py
@@ -1,5 +1,15 @@
-from module_structure.tensor_operator.py import *
+from module_structure1.tensor_operator import *
 
 
+__all__ = tensor_operator.__all__
 
-__all__ = tensor.op
+MAJOR = 1
+MINOR = 0
+PATCH = 0
+COMMIT = 12
+
+# from main import *
+
+__version__ = f'{MAJOR}.{MINOR}.{PATCH}.{COMMIT}'
+__author__  = 'Universidad Francisco de Vitoria'
+__name__    = 'module_structure1'
